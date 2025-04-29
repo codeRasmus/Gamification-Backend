40f5be085d3e274b82758bb872a1991dd04be95e
Rasmus <rasmusmllr@gmail.com>
2025-04-29 17:07:04 +0200

    changee schema and added log for testing for outgoing req.body contents

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -11,13 +11,16 @@ exports.getTaskById = async (req, res) => {
 };
 
 exports.createTask = async (req, res) => {
+  console.log(req.body);
   const task = new Task(req.body);
   await task.save();
   res.status(201).json(task);
 };
 
 exports.updateTask = async (req, res) => {
-  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
   res.json(task);
 };
 
