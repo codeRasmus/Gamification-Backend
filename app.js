8187db2e84c82c60f71af3bd636772069ba19a7b
Andreas Skovsted <as.kristiansen10@hotmail.com>
2025-05-02 08:58:26 +0200

    Added admin controller, model and routes

--- app.js
+++ app.js
@@ -2,22 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const taskRoutes = require("./routes/task.routes");
 // const teamRoutes = require("./routes/team.routes");
-// const adminRoutes = require("./routes/admin.routes");
+const adminRoutes = require("./routes/admin.routes");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-// Routes
 app.use("/api/tasks", taskRoutes);
 // app.use("/api/teams", teamRoutes);
-// app.use("/api/admin", adminRoutes);
+app.use("/api/admin", adminRoutes);
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+    app.listen(5500, () => console.log("Server running on http://localhost:5500"));
   })
   .catch((err) => console.error("MongoDB connection error:", err));
---
+++ controllers/admin.controller.js
@@ -0,0 +1,58 @@
+const Admin = require("../models/admin.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingAdmin = await Admin.findOne({ username });
+    if (existingAdmin) {
+      return res.status(409).json({ message: "Admin already exists" });
+    }
+
+    const saltRounds = parseInt(process.env.SALTROUNDS) || 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const admin = new Admin({ username, passwordHash });
+    await admin.save();
+
+    res.status(201).json({ message: "Admin created successfully" });
+  } catch (err) {
+    console.error("Error creating admin:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ username });
+    if (!admin) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, admin.passwordHash);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const token = jwt.sign({ id: admin._id, username: admin.username }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ token });
+  } catch (err) {
+    console.error("Error during login:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.getAllAdmins = async (req, res) => {
+  try {
+    const admins = await Admin.find().select("-passwordHash");
+    res.json(admins);
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
